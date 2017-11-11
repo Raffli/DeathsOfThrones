aeb79ae5e0c629d1e3d3df2997d206a96d0cc282
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-11-11 13:29:18 +0100

    MurderController angepasst

--- SpringBoot/src/main/java/deathOfThrones/rest/MurderController.java
+++ SpringBoot/src/main/java/deathOfThrones/rest/MurderController.java
@@ -1,5 +1,22 @@
 package deathOfThrones.rest;
 
-public class MurderController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import deathOfThrones.data.entities.MurderEntitie;
+import deathOfThrones.data.repositories.MurderRepository;
+
+@RequestMapping(path="/dot/murder")
+@RestController
+public class MurderController {
+	@Autowired
+	private MurderRepository murderRepository;
+	
+	@GetMapping(path="/allMurders")
+	public Iterable<MurderEntitie> getAllMurders() {
+		return murderRepository.findAll();
+	}
+	
 }
