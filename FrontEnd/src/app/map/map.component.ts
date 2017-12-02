afe1042df9f99ade9c642c0dad7f92e5dd3961fc
Florian Voelkers <florian.voelkers@mail.de>
2017-12-02 13:31:18 +0100

    Bewegen der Map mit der linken Maustaste bei verschiedenen scalings möglich

--- FrontEnd/src/app/map/map.component.ts
+++ FrontEnd/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DataService} from '../services/data-service.service';
 
@@ -10,12 +10,17 @@ import {DataService} from '../services/data-service.service';
 export class MapComponent implements OnInit {
 
   private mouseIsDown : boolean;
+  private startX : number;
+  private startY : number;
   private currentX : number;
   private currentY : number;
-  private positionX : string;
-  private positionY : string;
+  private positionX : number;
+  private positionY : number;
+  private offsetX : number;
+  private offsetY : number;
   private imageSource: string;
   private zoom: number;
+  private scaling : string;
   private screenWidth: number;
   private screenHeight: number;
   private imageWidth: number;
@@ -27,14 +32,20 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     this.mouseIsDown = false;
-    this.currentX = 0;
-    this.currentY = 0;
-    this.zoom = 100;
-    this.imageSource = '/assets/map/gotMap' + this.zoom + '.jpg';
-    this.screenWidth = window.innerWidth;
-    this.screenHeight = window.innerHeight - 25;
     this.imageHeight = 3682;
     this.imageWidth = 5652;
+    this.currentX = this.startX = 0;
+    this.currentY = this.startY = 0;
+    this.zoom = 0.75;
+    this.scaling = 'scale(' + this.zoom + ')';
+    this.imageSource = '/assets/map/gotMap100.jpg';
+    this.offsetX = -(this.imageWidth - this.imageWidth * this.zoom) * 0.5;
+    this.offsetY = -(this.imageHeight - this.imageHeight * this.zoom) * 0.5;
+    this.positionX = this.offsetX;
+    this.positionY = this.offsetY;
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight - 25;
+
 
     this.dataService.getDeathsByEpisode(60).subscribe(
       (data: any) => {
@@ -45,20 +56,43 @@ export class MapComponent implements OnInit {
 
   onMouseMove = function (event) {
     if (this.mouseIsDown) {
-      //console.log(event.clientX);
+      this.currentX += event.clientX - this.startX;
+      if (this.currentX < 0 && this.currentX > -1 * (this.imageWidth * this.zoom - this.screenWidth)) {
+        this.positionX = this.offsetX + this.currentX;
+      } else {
+        if (this.currentX >= 0) {
+          this.currentX = 0;
+        } else {
+          this.currentX = -1 * (this.imageWidth * this.zoom - this.screenWidth);
+        }
+      }
+      this.startX = event.clientX;
+      this.currentY += event.clientY - this.startY;
+      this.startY = event.clientY;
+      if (this.currentY < 0 && this.currentY > -1 * (this.imageHeight * this.zoom - this.screenHeight)) {
+        this.positionY = this.offsetY + this.currentY;
+      } else {
+        if (this.currentY >= 0) {
+          this.currentY = 0;
+        } else {
+          this.currentY = -1 * (this.imageHeight * this.zoom - this.screenHeight);
+        }
+      }
+      this.startY = event.clientY;
     }
   };
 
   onMouseUp = function (event) {
     this.mouseIsDown = false;
-    //console.log(this.mouseIsDown);
   };
 
   onMouseDown = function (event) {
     this.mouseIsDown = true;
-    //console.log(this.mouseIsDown);
+    this.startX = event.clientX;
+    this.startY = event.clientY;
   };
 
+  @HostListener('window:keydown', ['$event'])
   myKeyEvent = function (event) {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight - 25;
@@ -69,33 +103,32 @@ export class MapComponent implements OnInit {
       if (this.currentX < 0)
         this.currentX += 50;
     } else if (event.keyCode === 83 || event.keyCode === 40) { // s
-      let movableHeight = this.imageHeight * this.zoom / 100 - this.screenHeight;
+      let movableHeight = this.imageHeight * this.zoom - this.screenHeight;
       if ( (movableHeight + this.currentY) > 0 )
         this.currentY -= 50;
     } else if (event.keyCode === 68 || event.keyCode === 39) { // d
-      let movableWidth = this.imageWidth * this.zoom / 100 - this.screenWidth;
+      let movableWidth = this.imageWidth * this.zoom - this.screenWidth;
       console.log(movableWidth + " " + this.currentX);
       if ( (movableWidth + this.currentX) > 50 )
         this.currentX -= 50;
     } else if (event.keyCode === 109) {
       if (this.zoom > 35) {
         this.zoom -= 5;
+
       } else {
         return;
       }
-      this.imageSource = '/assets/map/gotMap' + this.zoom + '.jpg';
     } else if (event.keyCode === 107) {
       if (this.zoom < 150) {
         this.zoom += 5;
       } else {
         return;
       }
-      this.imageSource = '/assets/map/gotMap' + this.zoom + '.jpg';
     } else {
       return;
     }
-    this.positionX = this.currentX + 'px';
-    this.positionY = this.currentY + 'px';
+    this.positionX = this.currentX;
+    this.positionY = this.currentY;
   };
 
 }
